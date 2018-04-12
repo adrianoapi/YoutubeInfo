19f283f6ff793835acdfa1c734ffc9e5004b26e3
adrianometalslug <sdcomputadores@gmail.com>
2018-04-12 15:34:57 -0300

    update information video

--- yt.php
+++ yt.php
@@ -26,7 +26,15 @@ class YoutubeInfo {
     {
         return $size == "big" ?
                 'https://i.ytimg.com/vi/'  . $this->token .'/hqdefault.jpg' :
-                'https://i1.ytimg.com/vi/' . $this->token . '/default.jpg ';
+                'https://i1.ytimg.com/vi/' . $this->token . '/default.jpg ' ;
+    }
+    
+    public function getInfo($type = "json")
+    {
+        $data = $this->connect($type);
+        return $type == "json" ?
+                        json_decode($data) :
+                        simplexml_load_string($data);
     }
     
     public function getUrl()
@@ -50,11 +58,30 @@ class YoutubeInfo {
         $this->token = $array[1];
     }
     
+    private function connect($type)
+    {
+        $youtube = "https://www.youtube.com/oembed?url={$this->url}&format={$type}";
+        $options = array(
+            'http' => array(
+                'method' => 'GET',
+                'header' => "format: UTF-8\r\n"
+                . "User-Agent: PHP\r\n"
+                . "Connection: Connection: Keep-Alive\r\n"
+            )
+        );
+        $yt_options = stream_context_create($options);
+        return file_get_contents($youtube, false, $yt_options);
+    }
+    
 }
 
-# IMPLEMENTAR
+# IMPLEMENT
 $obj = new YoutubeInfo("https://www.youtube.com/watch?v=VHxjyBtKxpM");
 echo "<img src=\"{$obj->getThumb()}\">";
 echo "<img src=\"{$obj->getThumb('small')}\">";
 
+# INFORMATION
+echo "<pre>";
+print_r($obj->getInfo());
+echo "</pre>";
 ?>
