175eba0be8c6dc77e6758e7ff7c6f84a8cb49e68
adrianometalslug <sdcomputadores@gmail.com>
2018-04-12 14:38:58 -0300

    url

--- yt.php
+++ yt.php
@@ -4,6 +4,13 @@
 class YoutubeInfo {
     
     private $url;
+    private $token;
+    
+    public function __construct($url = NULL)
+    {
+        $this->url = $url;
+        $this->token = $this->setToken();
+    }
     
     public function setUrl($url = NULL)
     {
@@ -13,10 +20,10 @@ class YoutubeInfo {
     /* Get Thumb video youtube */
     public function getThumb()
     {
-        
+        return 'https://i.ytimg.com/vi/'. $this->token .'/hqdefault.jpg';
     }
     
-    public function getToken()
+    public function setToken()
     {
         $array = explode("=", $this->url);
         return $array[1];
@@ -25,7 +32,6 @@ class YoutubeInfo {
 }
 
 # IMPLEMENTAR
-$obj = new YoutubeInfo();
-$obj->setUrl("https://www.youtube.com/watch?v=VHxjyBtKxpM");
-var_dump($obj->getToken());
+$obj = new YoutubeInfo("https://www.youtube.com/watch?v=VHxjyBtKxpM");
+echo "<img src=\"{$obj->getThumb()}\">";
 ?>
