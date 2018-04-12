0c01a07200c656ad01b7adfe8d11f1b9c0dc7e75
adrianometalslug <sdcomputadores@gmail.com>
2018-04-12 14:24:10 -0300

    extraindo o token da url

--- yt.php
+++ yt.php
@@ -3,12 +3,29 @@
 
 class YoutubeInfo {
     
+    private $url;
+    
+    public function setUrl($url = NULL)
+    {
+        $this->url = $url;
+    }
+    
     /* Get Thumb video youtube */
     public function getThumb()
     {
         
     }
     
+    public function getToken()
+    {
+        $array = explode("=", $this->url);
+        return $array[1];
+    }
+    
 }
 
+# IMPLEMENTAR
+$obj = new YoutubeInfo();
+$obj->setUrl("https://www.youtube.com/watch?v=VHxjyBtKxpM");
+var_dump($obj->getToken());
 ?>
